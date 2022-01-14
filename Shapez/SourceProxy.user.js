6952dc6c2a015b0b64e875292e7fa5d09dbb3259
FatCatX <garretsimpson@yahoo.com>
2022-01-14 15:08:40 -0800

    Create SourceProxy.user.js
    Initial version

---
+++ Shapez/SourceProxy.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Source Proxy
+// @version      0.1
+// @description  Proxy to the source
+// @author       Me
+// @match        *://*.shapez.io/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @require      https://unpkg.com/lz-string@1.4.4/libs/lz-string.js
+// @require      file:///C:/users/garre/Projects/userscripts/Hooks.js
+// @require      file:///C:/users/garre/Projects/userscripts/Shapez/ShapezCopyAndPaste.user.js
+// @grant        unsafeWindow
+// ==/UserScript==
+(() => {})();
