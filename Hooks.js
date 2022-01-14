a465b344e93a0966aadd2bf7560921c085f1b5a8
FatCatX <garretsimpson@yahoo.com>
2022-01-14 08:56:57 -0800

    Create Hooks.js
    Initial version

---
+++ Hooks.js
@@ -0,0 +1,77 @@
+/**
+ * Hooks - Add functions to running scripts
+ *
+ * Hook object syntax
+ *   {
+ *     <class-name>: {
+ *       <hook-function>: {
+ *         func: <new-function>, // optional
+ *         adds: [<add-function>, ...] // optional
+ *       }, ...
+ *     }
+ *   }
+ *
+ */
+
+const SCRIPT_NAME = "Hooks";
+
+function log(...args) {
+  console.debug(SCRIPT_NAME, ...args);
+}
+
+const INTERNALS = {};
+
+export function getInternals() {
+  return INTERNALS;
+}
+
+export function createHooks(hooks) {
+  const { defineProperty, prototype } = unsafeWindow.Object;
+  const cNames = Object.keys(hooks);
+  for (const cName of cNames) {
+    const hookNames = Object.keys(hooks[cName]);
+    for (const hookName of hookNames) {
+      const hook = hooks[cName][hookName];
+      log("Adding hook:", cName, hookName);
+      const funcList = [];
+      defineProperty(prototype, hookName, {
+        __proto__: null,
+        configurable: true,
+        set: function (oldFunc) {
+          log("Hook callback:", cName, hookName);
+          const fName = this.constructor.name;
+          const found = fName == cName || fName == "_" + cName;
+          if (!found) {
+            log("  Skip:", fName, oldFunc.name);
+            funcList.push([this, oldFunc]);
+            return;
+          }
+          log("  Found:", fName, oldFunc.name);
+          INTERNALS[cName] = this.constructor;
+          delete prototype[hookName];
+          let func =
+            hook.func == undefined
+              ? oldFunc
+              : function (...args) {
+                  return hook.func.call(this, oldFunc, ...args);
+                };
+          funcList.push([this, func]);
+          for (const [proto, func] of funcList) {
+            defineProperty(proto, hookName, {
+              configurable: true,
+              enumerable: true,
+              writable: true,
+              value: func,
+            });
+          }
+          if (hook.adds != undefined) {
+            for (let addFunc of hook.adds) {
+              log("  Adding:", addFunc.name);
+              defineProperty(this, addFunc.name, { value: addFunc });
+            }
+          }
+        },
+      });
+    }
+  }
+}
